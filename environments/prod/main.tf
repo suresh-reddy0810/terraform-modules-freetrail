d6b2e51686fb06b375e4f5996dab40aa7c941614
suresh-reddy0810 <pernatisureshreddy2002@gmail.com>
2026-03-16 23:19:08 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,10 +2,17 @@ module "rg" {
     source = "../../modules/rg/"
     rg_name = var.rg_name
     location = var.location
+    rg_aks_name = var.rg_aks_name
+    aks_location = var.aks_location
 }
 
 module "vnet" {
     source = "../../modules/vnet/"
+    rg_name = module.rg.rg_name
+    location = module.rg.location
+    rg_aks_name = module.rg.rg_aks_name
+    aks_location = module.rg.aks_location
+
     vnet_name = var.vnet_name
     vnet_cicdr = var.vnet_cicdr
     sub1_name = var.sub1_name
@@ -13,26 +20,30 @@ module "vnet" {
     sub2_name = var.sub2_name
     sub2_cicdr = var.sub2_cicdr
     nsg_name = var.nsg_name
-    rg_name = module.rg.rg_name
-    location = module.rg.location
+    vnet1_name = var.vnet1_name
+    vnet1_cicdr = var.vnet1_cicdr
+    sub_name = var.sub_name
+    sub_cicdr = var.sub_cicdr
+    
 }
+
 
 module "vm" {
     source = "../../modules/vm/"
     vm_name = var.vm_name
     nic_name = var.nic_name
-    sub1_id = module.vnet.sub1_id
     rg_name = module.rg.rg_name
     location = module.rg.location
+    sub1_id = module.vnet.sub1_id
 
 }
 
 module "storage" {
     source = "../../modules/storage/"
     storage_account_name = var.storage_account_name
+    sub2_id = module.vnet.sub2_id
     rg_name = module.rg.rg_name
     location = module.rg.location
-    sub2_private_id = module.vnet.sub2_private_id
 }
 
 module "webapp" {
@@ -43,15 +54,13 @@ module "webapp" {
     location = module.rg.location
 }
 
+
 module "aks" {
-    source = "../../modules/aks/"
-    aks_name = var.aks_name
-    sub1_name = module.vnet.sub1_name
-    rg_name = module.rg.rg_name
-    location = module.rg.location
+  source = "../../modules/aks/"
 
+  aks_name      = var.aks_name
+  sub_aks_id    = module.vnet.sub_aks_id
+
+  rg_aks_name   = module.rg.rg_aks_name
+  aks_location  = module.rg.aks_location
 }
-
-
-
-
